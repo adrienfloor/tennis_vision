ece76da1db5fcb26f8f83945e1aed887a4bfe3ae
adrien floor <adrienfloor@gmail.com>
2022-12-07 16:21:28 +0100

    adancements on frontend

--- tennis-tracking/full_tracking.py
+++ tennis-tracking/full_tracking.py
@@ -349,6 +349,14 @@ fps = int(video.get(cv2.CAP_PROP_FPS))
 length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
+# output_video = cv2.VideoCapture(output_video_path)
+
+# output_width = int(output_video.get(cv2.CAP_PROP_FRAME_WIDTH))
+# output_height = int(output_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# fps = int(output_video.get(cv2.CAP_PROP_FPS))
+# length = int(output_video.get(cv2.CAP_PROP_FRAME_COUNT))
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
 print(fps)
 print(length)
 
@@ -367,37 +375,64 @@ extremaTop = [
     [xMiddleLeft,yMiddleLeft]
 ]
 score = ''
+bounce_count = 0
+call = ''
 while True:
   ret, frame = video.read()
-  call = ''
+  fed = cv2.imread('./images/fed-nobg.png')
+  nad = cv2.imread('./images/nad-nobg.png')
+
+  scale_percent = 420 # percent of original size
+  fed_width = int(fed.shape[1] * scale_percent / 100)
+  fed_height = int(fed.shape[0] * scale_percent / 100)
+  fed_dim = (fed_width, fed_height)
+
+  # resize image
+  federer = cv2.resize(fed, fed_dim, interpolation = cv2.INTER_AREA)
+
+  scale_percent = 420 # percent of original size
+  nad_width = int(nad.shape[1] * scale_percent / 100)
+  nad_height = int(nad.shape[0] * scale_percent / 100)
+  nad_dim = (nad_width, nad_height)
+
+  # resize image
+  nadal = cv2.resize(nad, nad_dim, interpolation = cv2.INTER_AREA)
+
   if ret:
-    # if coords[i] is not None:
-    # color_ = (0,128,0)
-    # cv2.rectangle(frame,(100,50),(300,128),(255,255,255),-1)
-    # cv2.putText(frame,call,(178,126),cv2.FONT_HERSHEY_SIMPLEX,3,color_,10,cv2.LINE_AA)
-    call = ''
     color_ = (0,128,0)
-    cv2.rectangle(frame,(70,400),(250,528),(255,255,255),-1)
-    cv2.putText(frame,call,(108,496),cv2.FONT_HERSHEY_SIMPLEX,3,color_,10,cv2.LINE_AA)
+    cv2.rectangle(frame,(0,0),(580,150),(255,255,255),-1)
+    cv2.putText(frame,f'Bounces:{bounce_count}',(10,100),cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,0),10,cv2.LINE_AA)
+    # cv2.putText(frame,call,(60,100),cv2.FONT_HERSHEY_SIMPLEX,3,color_,10,cv2.LINE_AA)
     if i in idx:
-      center_coordinates = int(xy[i][0]), int(xy[i][1])
       color = (255, 0, 0)
       thickness = -1
+      if i-1 not in idx:
+        bounce_count = bounce_count + 1
 
       ex = extremaBottom if int(xy[i][1]) > yMiddleLeft else extremaTop
+      center_coordinates = int(xy[i][0]), int(xy[i][1])
       call = judge_ball(ex, center_coordinates)
 
       cv2.circle(frame, center_coordinates, 10, color, thickness)
       color_ = (0,128,0) if call == 'IN' else (0, 0, 255)
-      cv2.putText(frame,call,(108,496),cv2.FONT_HERSHEY_SIMPLEX,3,color_,10,cv2.LINE_AA)
-    elif i-1 in idx or i-2 in idx or i-3 in idx or i-4 in idx or i-5 in idx:
+      img = nadal if call == 'IN' else federer
+      cv2.putText(frame,f'Bounces:{bounce_count}',(10,100),cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,0),10,cv2.LINE_AA)
+      output = merge(frame, img)
+      output_video.write(output)
+      #cv2.putText(frame,call,(60,100),cv2.FONT_HERSHEY_SIMPLEX,3,color_,10,cv2.LINE_AA)
+    elif i-1 in idx or i-2 in idx or i-3 in idx or i-4 in idx or i-5 in idx or i-6 in idx or i-7 in idx or i-8 in idx or i-9 in idx or i-10 in idx:
       color_ = (0,128,0) if call == 'IN' else (0, 0, 255)
-      cv2.putText(frame,call,(198,496),cv2.FONT_HERSHEY_SIMPLEX,3,color_,10,cv2.LINE_AA)
+      #call = judge_ball(ex, center_coordinates)
+      img = nadal if call == 'IN' else federer
+      cv2.putText(frame,f'Bounces:{bounce_count}',(10,100),cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,0),10,cv2.LINE_AA)
+      output = merge(frame, img)
+      output_video.write(output)
+      #cv2.putText(frame,call,(60,100),cv2.FONT_HERSHEY_SIMPLEX,3,color_,10,cv2.LINE_AA)
     else:
-      call = ''
+      cv2.putText(frame,f'Bounces:{bounce_count}',(10,100),cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,0),10,cv2.LINE_AA)
+      output_video.write(frame)
+      #call = ''
     i += 1
-
-    output_video.write(frame)
   else:
     break
 
